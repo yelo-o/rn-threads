6b8d5f5facd8d9d0a7ce84d72ba9ee61f3907f14
yelo-o <flyordig@gmail.com>
2025-07-05 21:02:27 +0900

    Add: 기본 탭들 추가

---
+++ app/modal.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from "expo-router";
+import { Pressable, Text, View } from "react-native";
+
+export default function Modal() {
+    const router = useRouter();
+    return (
+        <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+            <Text>I am a modal</Text>
+            <Pressable onPress={() => router.back()}>
+                <Text>Close</Text>
+            </Pressable>
+        </View>
+    );
+}
