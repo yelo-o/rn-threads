d660ac5a76ae7b56d8e5922aedea7f121bd3068b
yelo-o <flyordig@gmail.com>
2025-07-06 15:48:54 +0900

    Update: Activity 탭 눌렀을 떄 한 페이지만 있었으나, 여러 인덱스를 담고 있는 페이지를 노출하도록 수정

---
+++ app/(tabs)/activity/index.tsx
@@ -0,0 +1,96 @@
+import { Text, View, TouchableOpacity } from "react-native";
+import { usePathname, useRouter } from "expo-router";
+import NotFound from "@/app/+not-found";
+
+export default function Index() {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    if (
+        ![
+            "/activity",
+            "/activity/follows",
+            "/activity/replies",
+            "/activity/mentions",
+            "/activity/quotes",
+            "/activity/reposts",
+            "/activity/verified",
+        ].includes(pathname)
+    ) {
+        return <NotFound />;
+    }
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <View>
+                <TouchableOpacity
+                    onPress={() => {
+                        router.push(`/activity`);
+                    }}
+                >
+                    <Text>All</Text>
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity
+                    onPress={() => {
+                        router.push(`/activity/follows`);
+                    }}
+                >
+                    <Text>Follows</Text>
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity
+                    onPress={() => {
+                        router.push(`/activity/replies`);
+                    }}
+                >
+                    <Text>Replies</Text>
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity
+                    onPress={() => {
+                        router.push(`/activity/mentions`);
+                    }}
+                >
+                    <Text>Mentions</Text>
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity
+                    onPress={() => {
+                        router.push(`/activity/quotes`);
+                    }}
+                >
+                    <Text>Quotes</Text>
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity
+                    onPress={() => {
+                        router.push(`/activity/reposts`);
+                    }}
+                >
+                    <Text>Reposts</Text>
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity
+                    onPress={() => {
+                        router.push(`/activity/verified`);
+                    }}
+                >
+                    <Text>Verified</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
