81840d1c53324a6a17dbf267527c2dd2db5754c2
yelo-o <flyordig@gmail.com>
2025-07-14 23:03:38 +0900

    Update: 스타일 작업 수정

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,68 +1,91 @@
-import { Text, View, TouchableOpacity } from "react-native";
 import { usePathname, useRouter } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
     const router = useRouter();
     const pathname = usePathname();
+    const insets = useSafeAreaInsets();
 
     console.log("pathname >> ", pathname);
+    console.log("insets: ", insets);
+
     return (
         <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
+            style={[
+                styles.container,
+                { paddingTop: insets.top, paddingBottom: insets.bottom },
+            ]}
         >
-            <View>
-                <TouchableOpacity
-                    onPress={() => {
-                        router.replace(`/`);
-                    }}
-                >
-                    <Text style={{ color: pathname === "/" ? "red" : "black" }}>
-                        For you
-                    </Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TouchableOpacity
-                    onPress={() => {
-                        router.replace(`/following`);
-                    }}
-                >
-                    <Text style={{ color: pathname === "/" ? "black" : "red" }}>
-                        Following
-                    </Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TouchableOpacity
-                    onPress={() => {
-                        router.push(`/@kmg/post/1`);
-                    }}
-                >
-                    <Text>게시글1</Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TouchableOpacity
-                    onPress={() => {
-                        router.push(`/@kmg/post/2`);
-                    }}
-                >
-                    <Text>게시글2</Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TouchableOpacity
-                    onPress={() => {
-                        router.push(`/@kmg/post/3`);
-                    }}
-                >
-                    <Text>게시글3</Text>
-                </TouchableOpacity>
+            <View style={styles.tabContainer}>
+                <View style={styles.tab}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            router.replace(`/`);
+                        }}
+                    >
+                        <Text
+                            style={{
+                                color: pathname === "/" ? "red" : "black",
+                            }}
+                        >
+                            For you
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity
+                        onPress={() => {
+                            router.replace(`/following`);
+                        }}
+                    >
+                        <Text
+                            style={{
+                                color: pathname === "/" ? "black" : "red",
+                            }}
+                        >
+                            Following
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity
+                        onPress={() => {
+                            router.push(`/@kmg/post/1`);
+                        }}
+                    >
+                        <Text>게시글1</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity
+                        onPress={() => {
+                            router.push(`/@kmg/post/2`);
+                        }}
+                    >
+                        <Text>게시글2</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity
+                        onPress={() => {
+                            router.push(`/@kmg/post/3`);
+                        }}
+                    >
+                        <Text>게시글3</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    tabContainer: {
+        flexDirection: "row",
+    },
+    tab: { flex: 1 },
+});
