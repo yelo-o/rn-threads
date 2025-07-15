89650f37c93d3a4182dd5da0fd942c46cd5df0fb
yelo-o <flyordig@gmail.com>
2025-07-15 22:10:52 +0900

    Update: Dimensions(너비와 높이 값 측정), For you와 Following은 로그인했을 때만 나오도록 함

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,14 +1,27 @@
+import { BlurView } from "expo-blur";
 import { usePathname, useRouter } from "expo-router";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+    Dimensions,
+    Image,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
     const router = useRouter();
     const pathname = usePathname();
     const insets = useSafeAreaInsets();
+    const isLoggedIn = false;
 
     console.log("pathname >> ", pathname);
     console.log("insets: ", insets);
+
+    const { width, height } = Dimensions.get("window");
+
+    console.log(`화면 너비: ${width}dp, 높이: ${height}dp`);
 
     return (
         <View
@@ -17,37 +30,52 @@ export default function Index() {
                 { paddingTop: insets.top, paddingBottom: insets.bottom },
             ]}
         >
-            <View style={styles.tabContainer}>
-                <View style={styles.tab}>
+            <BlurView style={styles.header} intensity={70}>
+                <Image source={require("@/assets/images/react-logo.png")} />
+                {!isLoggedIn && (
                     <TouchableOpacity
-                        onPress={() => {
-                            router.replace(`/`);
-                        }}
+                        style={styles.loginButton}
+                        onPress={() => router.navigate("/login")}
                     >
-                        <Text
-                            style={{
-                                color: pathname === "/" ? "red" : "black",
+                        <Text style={styles.loginButtonText}>로그인</Text>
+                    </TouchableOpacity>
+                )}
+            </BlurView>
+            {isLoggedIn && (
+                <View style={styles.tabContainer}>
+                    <View style={styles.tab}>
+                        <TouchableOpacity
+                            onPress={() => {
+                                router.replace(`/`);
                             }}
                         >
-                            For you
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-                <View>
-                    <TouchableOpacity
-                        onPress={() => {
-                            router.replace(`/following`);
-                        }}
-                    >
-                        <Text
-                            style={{
-                                color: pathname === "/" ? "black" : "red",
+                            <Text
+                                style={{
+                                    color: pathname === "/" ? "red" : "black",
+                                }}
+                            >
+                                For you
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <TouchableOpacity
+                            onPress={() => {
+                                router.replace(`/following`);
                             }}
                         >
-                            Following
-                        </Text>
-                    </TouchableOpacity>
+                            <Text
+                                style={{
+                                    color: pathname === "/" ? "black" : "red",
+                                }}
+                            >
+                                Following
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
+            )}
+            <View>
                 <View>
                     <TouchableOpacity
                         onPress={() => {
@@ -88,4 +116,23 @@ const styles = StyleSheet.create({
         flexDirection: "row",
     },
     tab: { flex: 1 },
+    header: { alignItems: "center" },
+    headerLogo: {
+        width: 42,
+        height: 42,
+    },
+    loginButton: {
+        position: "absolute",
+        right: 10,
+        top: 0,
+        backgroundColor: "black",
+        borderWidth: 1,
+        borderColor: "black",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 10,
+    },
+    loginButtonText: {
+        color: "white",
+    },
 });
